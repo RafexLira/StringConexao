a524d5ff7b6530f9d9a03bfebfd90e14eb65bebf
Rafael Lira <rafael.alexandrelira@hotmail.com>
2020-09-04 01:27:08 -0300

    Conexão c# SQL

---
+++ StringConexao/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace StringConexao
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.BtnBuscar = new System.Windows.Forms.Button();
+            this.TxtNome = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BtnIncluir = new System.Windows.Forms.Button();
+            this.TxtTelefone = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nomeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.telefoneDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clienteBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.testeDataSet1 = new StringConexao.TesteDataSet1();
+            this.clienteTableAdapter = new StringConexao.TesteDataSet1TableAdapters.ClienteTableAdapter();
+            this.BtnAlterar = new System.Windows.Forms.Button();
+            this.BtnProcurar = new System.Windows.Forms.Button();
+            this.TxtTelefoneCli = new System.Windows.Forms.TextBox();
+            this.TxtCliente = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.clienteBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.testeDataSet1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BtnBuscar
+            // 
+            this.BtnBuscar.Location = new System.Drawing.Point(437, 201);
+            this.BtnBuscar.Name = "BtnBuscar";
+            this.BtnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.BtnBuscar.TabIndex = 0;
+            this.BtnBuscar.Text = "Buscar";
+            this.BtnBuscar.UseVisualStyleBackColor = true;
+            this.BtnBuscar.Click += new System.EventHandler(this.BtnBuscar_Click);
+            // 
+            // TxtNome
+            // 
+            this.TxtNome.Location = new System.Drawing.Point(203, 65);
+            this.TxtNome.Name = "TxtNome";
+            this.TxtNome.Size = new System.Drawing.Size(100, 20);
+            this.TxtNome.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(153, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nome";
+            // 
+            // BtnIncluir
+            // 
+            this.BtnIncluir.Location = new System.Drawing.Point(217, 126);
+            this.BtnIncluir.Name = "BtnIncluir";
+            this.BtnIncluir.Size = new System.Drawing.Size(75, 23);
+            this.BtnIncluir.TabIndex = 4;
+            this.BtnIncluir.Text = "Incluir";
+            this.BtnIncluir.UseVisualStyleBackColor = true;
+            this.BtnIncluir.Click += new System.EventHandler(this.BtnIncluir_Click);
+            // 
+            // TxtTelefone
+            // 
+            this.TxtTelefone.Location = new System.Drawing.Point(203, 100);
+            this.TxtTelefone.Name = "TxtTelefone";
+            this.TxtTelefone.Size = new System.Drawing.Size(100, 20);
+            this.TxtTelefone.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(153, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Telefone";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.nomeDataGridViewTextBoxColumn,
+            this.telefoneDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.clienteBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(548, 266);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 8;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "Id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            this.idDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // nomeDataGridViewTextBoxColumn
+            // 
+            this.nomeDataGridViewTextBoxColumn.DataPropertyName = "Nome";
+            this.nomeDataGridViewTextBoxColumn.HeaderText = "Nome";
+            this.nomeDataGridViewTextBoxColumn.Name = "nomeDataGridViewTextBoxColumn";
+            // 
+            // telefoneDataGridViewTextBoxColumn
+            // 
+            this.telefoneDataGridViewTextBoxColumn.DataPropertyName = "Telefone";
+            this.telefoneDataGridViewTextBoxColumn.HeaderText = "Telefone";
+            this.telefoneDataGridViewTextBoxColumn.Name = "telefoneDataGridViewTextBoxColumn";
+            // 
+            // clienteBindingSource
+            // 
+            this.clienteBindingSource.DataMember = "Cliente";
+            this.clienteBindingSource.DataSource = this.testeDataSet1;
+            // 
+            // testeDataSet1
+            // 
+            this.testeDataSet1.DataSetName = "TesteDataSet1";
+            this.testeDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // clienteTableAdapter
+            // 
+            this.clienteTableAdapter.ClearBeforeFill = true;
+            // 
+            // BtnAlterar
+            // 
+            this.BtnAlterar.Location = new System.Drawing.Point(217, 167);
+            this.BtnAlterar.Name = "BtnAlterar";
+            this.BtnAlterar.Size = new System.Drawing.Size(75, 23);
+            this.BtnAlterar.TabIndex = 9;
+            this.BtnAlterar.Text = "Alterar";
+            this.BtnAlterar.UseVisualStyleBackColor = true;
+            this.BtnAlterar.Click += new System.EventHandler(this.BtnAlterar_Click);
+            // 
+            // BtnProcurar
+            // 
+            this.BtnProcurar.Location = new System.Drawing.Point(322, 65);
+            this.BtnProcurar.Name = "BtnProcurar";
+            this.BtnProcurar.Size = new System.Drawing.Size(75, 23);
+            this.BtnProcurar.TabIndex = 10;
+            this.BtnProcurar.Text = "Procurar";
+            this.BtnProcurar.UseVisualStyleBackColor = true;
+            this.BtnProcurar.Click += new System.EventHandler(this.BtnProcurar_Click);
+            // 
+            // TxtTelefoneCli
+            // 
+            this.TxtTelefoneCli.Location = new System.Drawing.Point(526, 100);
+            this.TxtTelefoneCli.Name = "TxtTelefoneCli";
+            this.TxtTelefoneCli.Size = new System.Drawing.Size(100, 20);
+            this.TxtTelefoneCli.TabIndex = 12;
+            // 
+            // TxtCliente
+            // 
+            this.TxtCliente.Location = new System.Drawing.Point(526, 65);
+            this.TxtCliente.Name = "TxtCliente";
+            this.TxtCliente.Size = new System.Drawing.Size(100, 20);
+            this.TxtCliente.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TxtTelefoneCli);
+            this.Controls.Add(this.TxtCliente);
+            this.Controls.Add(this.BtnProcurar);
+            this.Controls.Add(this.BtnAlterar);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TxtTelefone);
+            this.Controls.Add(this.BtnIncluir);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TxtNome);
+            this.Controls.Add(this.BtnBuscar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.clienteBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.testeDataSet1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnBuscar;
+        private System.Windows.Forms.TextBox TxtNome;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BtnIncluir;
+        private System.Windows.Forms.TextBox TxtTelefone;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private TesteDataSet1 testeDataSet1;
+        private System.Windows.Forms.BindingSource clienteBindingSource;
+        private TesteDataSet1TableAdapters.ClienteTableAdapter clienteTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nomeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn telefoneDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Button BtnAlterar;
+        private System.Windows.Forms.Button BtnProcurar;
+        private System.Windows.Forms.TextBox TxtTelefoneCli;
+        private System.Windows.Forms.TextBox TxtCliente;
+    }
+}
+
---
+++ StringConexao/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace StringConexao
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: esta linha de código carrega dados na tabela 'testeDataSet1.Cliente'. Você pode movê-la ou removê-la conforme necessário.
+            //this.clienteTableAdapter.Fill(this.testeDataSet1.Cliente);
+        }
+
+        string StrConn = @"Data Source= corei7\sqlexpress; Database=Teste;Integrated Security=SSPI;";
+        string ConsulTodos = "SELECT *FROM Cliente";
+        string Selecionar = "SELECT *FROM Cliente WHERE Nome = @Nome"; // Isto é só um exemplo, o correto seria pelo ID
+        string Incluir = "INSERT INTO Cliente (Nome, Telefone)VALUES(@Nome,@Telefone)";
+        string Atualizar = "UPDATE Cliente SET Nome = @Nome, Telefone = @Telefone WHERE Nome = @Nome";
+
+
+        private void BtnBuscar_Click(object sender, EventArgs e)
+        {
+
+            SqlConnection conn = new SqlConnection(StrConn);
+            SqlDataAdapter da = new SqlDataAdapter(ConsulTodos, conn);
+            DataSet ds = new DataSet();
+            try
+            {
+         
+            conn.Open();
+            da.Fill(ds);
+
+            dataGridView1.DataSource = ds.Tables[0];
+              
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+               
+            }
+
+        }
+        private void BtnProcurar_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(StrConn);
+            SqlCommand cmd = new SqlCommand(Selecionar, conn);           
+
+
+            try
+            {
+                conn.Open();
+                cmd.Parameters.AddWithValue("@Nome", TxtNome.Text);
+                SqlDataReader dr = cmd.ExecuteReader();              
+             
+                while (dr.Read())
+                {
+                    TxtCliente.Text = dr["Nome"].ToString();                
+                    TxtTelefoneCli.Text = dr["Telefone"].ToString();
+                  
+
+
+                }
+                dr.Close();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+        private void BtnIncluir_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(StrConn);
+            SqlCommand cmd = new SqlCommand(Incluir, conn);
+            try
+            {               
+                cmd.Parameters.AddWithValue("@Nome", TxtNome.Text);
+                cmd.Parameters.AddWithValue("@Telefone", TxtTelefone.Text.ToString());
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("incluído com sucesso!");
+            }
+            catch(SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void BtnAlterar_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(StrConn);
+            SqlCommand cmd = new SqlCommand(Atualizar, conn);
+            try
+            {
+                cmd.Parameters.AddWithValue("@Nome", TxtNome.Text);
+                cmd.Parameters.AddWithValue("@Telefone", TxtTelefone.Text.ToString());
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Atualizado com sucesso!");
+                
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+        }
+
+       
+    }
+}
+/*
+ *  SqlConnection conn = new SqlConnection(StrConn);
+            SqlCommand cmd = new SqlCommand(ConsulTodos, conn);
+            DataSet ds = new DataSet();
+
+
+            try
+            {
+                conn.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+
+                    listBox1.Items.Add(dr["Nome"]).ToString();
+                    listBox1.Items.Add(dr["Telefone"]).ToString();
+                 
+                   
+                }
+                dr.Close();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+ 
+     */
